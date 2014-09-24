557a3e0e9068885e5f7657a7629aea392c7e0115
James Kreft <jkreft@usgs.gov>
2014-09-24 16:09:24 -0500

    major refactor of project organization

--- app/PubsFlask.py
+++ app/PubsFlask.py
@@ -1,30 +1,58 @@
 
-from flask import Flask, render_template, abort, request, jsonify
+from flask import Flask, render_template, abort, request, jsonify, Response
 from requests import get
 from webargs import Arg
 from webargs.flaskparser import FlaskParser
+import json
+from utils import pubdetails, pull_feed
 
-from Stubdata import pubs
 
 app = Flask(__name__)
 
-stubbed = True
-baseApiUrl = "http://127.0.0.1:5000/api"
+pub_url = "http://127.0.0.1:5001/api/"
+lookup_url = "https://pubs-test.er.usgs.gov/pubs-services/lookup/"
 
-def Getdata(indexId):
-    if stubbed == True:
-        pub = filter(lambda t: t['indexId'] == indexId, pubs)
-        pubreturn = {'pub': pub[0]}
 
+
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+#contact form
+@app.route('/contact')
+def contact():
+    return 'Contact form will live here'
+
+#leads to rendered html for publication page
+@app.route('/publication/<indexId>')
+def publication(indexId):
+    r = get(pub_url+'publication/'+indexId)
+    pubreturn = r.json()
+    pubdata= pubdetails(pubreturn['pub'])
+    return render_template('publication.html', indexID=indexId, pubdata=pubdata)
+
+#leads to json for selected endpoints
+@app.route('/lookup/<endpoint>')
+def lookup(endpoint):
+    endpoint_list = ['costcenters', 'publicationtypes', 'publicationsubtypes', 'publicationseries']
+    endpoint = endpoint.lower()
+    if endpoint in endpoint_list:
+        r = get(lookup_url+endpoint+'?mimetype=json').json()
+        return Response(json.dumps(r),  mimetype='application/json')
     else:
-        r = get(baseApiUrl+'/publication/'+indexId)
-        pubreturn = r.json()
-    return pubreturn
+        abort(404)
+
+@app.route('/faq')
+def faq():
+    feed_url = 'https://my.usgs.gov/confluence/createrssfeed.action?types=page&spaces=pubswarehouseinfo&title=myUSGS+4.0+RSS+Feed&labelString=pw_faq&excludedSpaceKeys%3D&sort=modified&maxResults=10&timeSpan=600&showContent=true&confirm=Create+RSS+Feed'
+    faq_content = pull_feed(feed_url)
+    print(faq_content)
+    return render_template('faq.html', faq_content=faq_content)
 
 
-
-
-
+#search args, will be used for the search params and generating the opensearch.xml documen
 search_args = {
     'title': Arg(str, multiple=True),
     'author': Arg(str, multiple=True),
@@ -32,53 +60,17 @@ search_args = {
     'abstract': Arg(str, multiple=True)
 }
 
-@app.route('/')
-def index():
-    return ' this is the index'
-
-
-
-#will lead to the rendered HTML for the object
-@app.route('/publication/<indexId>')
-def publication(indexId):
-    pubreturn = Getdata(indexId)
-    pubdata= pubreturn['pub']
-    return render_template('publication.html', indexID=indexId, pubdata=pubdata)
-
-@app.route('/api/publication', methods=['GET'])
-def get_pubs():
-    return jsonify({'pubs': pubs})
-
-@app.route('/api/publication/<indexId>', methods=['GET'])
-def get_pub(indexId):
-    pub = filter(lambda t: t['indexId'] == indexId, pubs)
-    if len(pub) == 0:
-        abort(404)
-    return jsonify({'pub': pub[0]})
-
-#this is a basic built in flask args tool, where the arguments don't actually have to be described, but then they also
-# can't exist more than once, etc
-@app.route('/api/search', methods=['GET'])
-def api_hello():
-    params = request.args.items()
-    print params
-    if 'indexID' in request.args:
-        indexId = request.args['indexId']
-        pub = filter(lambda t: t['indexId'] == indexId, pubs)
-        if len(pub) == 0:
-            abort(404)
-        return jsonify({'pub': pub[0]})
-    else:
-        return jsonify({'pubs': pubs})
 
 #this takes advantage of the webargs package, which allows for multiple parameter entries. e.g. year=1981&year=1976
-@app.route('/api/searchwebargs', methods=['GET'])
+@app.route('/search/searchwebargs', methods=['GET'])
 def api_webargs():
     parser = FlaskParser()
     args = parser.parse(search_args, request)
 
     print 'webarg param: ', args
     #TODO: map the webargs to the Pubs Warehouse Java API, generate output
+
+
 
 
 if __name__ == '__main__':
---
+++ app/utils.py
@@ -0,0 +1,80 @@
+__author__ = 'jameskreft'
+
+from requests import get
+import pprint
+import feedparser
+
+from BeautifulSoup import BeautifulSoup
+
+def call_api(baseapiurl, index_id):
+    r = get(baseapiurl+'/publication/'+index_id)
+    pubreturn = r.json()
+    pubdata= pubreturn['pub']
+    return pubdata
+
+def pubdetails(pubdata):
+    pubdata['details'] = []
+    detailslist = [
+        ['publicationType','text','Publication type:'],
+        ['publicationSubtype','text', 'Publication Subtype:'],
+        ['seriesName','Series name:'],
+        ['seriesNumber', 'Series number:'],
+        ['issn','online','ISSN (online):'],
+        ['issn','print', 'ISSN (print):'],
+        ['isbn', 'ISBN:'],
+        ['doi', 'DOI:'],
+        ['language', 'Language:'],
+        ['publisher', 'Publisher:'],
+        ['publisherLocation','Publisher location:'],
+        ['costCenters','text', 'Contributing office(s):'],
+        ['productDescription','Description:'],
+        ['numberofPages','Number of pages:'],
+        ['startPage','Start page:'],
+        ['endPage','End page:'],
+        ['temporalStart','Time Range Start'],
+        ['temporalEnd','Time Range End']
+    ]
+    for detail in detailslist:
+        if len(detail) == 3:
+            if pubdata.get(detail[0]) is not None and isinstance(pubdata.get(detail[0]), dict):
+                pubdata['details'].append({detail[2] : pubdata[detail[0]].get(detail[1])})
+            elif pubdata.get(detail[0]) is not None and isinstance(pubdata.get(detail[0]),list):
+                dd = ''
+                for det in pubdata.get(detail[0]):
+                    dd = dd+det.get(detail[1])+', '
+                dd=dd[:-2]
+                pubdata['details'].append({detail[2] : dd})
+        elif pubdata.get(detail[0]) is not None and len(pubdata.get(detail[0])) > 0 :
+            pubdata['details'].append({detail[1] : pubdata.get(detail[0])})
+    return pubdata
+
+
+
+
+
+def pull_feed (feed_url):
+    feed = feedparser.parse(feed_url)
+
+    # Process html to remove unwanted mark-up and fix links
+    post = ''
+    if len(feed['entries']) > 0:
+        soup = BeautifulSoup(feed['entries'][0].summary)
+
+        # Remove edited by paragraph
+        soup.p.extract()
+
+        # Remove final div in the feed
+        feedDiv = soup.find('div', class_='feed')
+        childrenDivs = feedDiv.findAll('div')
+        childrenDivs[len(childrenDivs) - 1].extract()
+
+        # Translate any in page links to use relative URL
+        base = feed['entries'][0].summary_detail.base
+        links = feedDiv.select('a[href^="' + base + '"]')
+        for link in links:
+            link['href'] = link['href'].replace(base, '')
+
+        post = unicode(soup)
+
+    return {'post' : post}
+
