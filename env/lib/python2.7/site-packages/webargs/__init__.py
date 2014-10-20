a0f9f728e0586f6685ecbdd275e4adaa05fa0280
Tomcat Application Server <tomcat@cidasddvaspubsw.cr.usgs.gov>
2014-10-20 10:20:39 -0500

    automatic inclusion of the env and static files

---
+++ env/lib/python2.7/site-packages/webargs/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.5.0'
+__author__ = 'Steven Loria'
+__license__ = "MIT"
+
+
+from webargs.core import Arg, WebargsError, ValidationError
+
+__all__ = ['Arg', 'WebargsError', 'ValidationError']
