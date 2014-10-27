b64c37b31cdfc57e5fbc836b1454de7f51fb5d03
Yan, Andrew N. <ayan@usgs.gov>
2014-10-27 12:21:29 -0500

    added files to index

---
+++ flask_utils/flask_pagination.py
@@ -0,0 +1,36 @@
+'''
+Created on Oct 27, 2014
+
+@author: ayan
+'''
+from math import ceil
+
+
+class Pagination(object):
+    
+    def __init__(self, page, per_page, total_count):
+        self.page = page
+        self.per_page = per_page
+        self.total_count = total_count
+        
+    @property
+    def pages(self):
+        ceiling = ceil(self.total_count / float(self.per_page))
+        return int(ceiling)
+    
+    @property
+    def has_prev(self):
+        return self.page > 1
+    
+    @property
+    def has_next(self):
+        return self.page < self.pages
+    
+    def iter_pages(self, left_edge=2, left_current=2, right_current=2, right_edge=2):
+        last = 0
+        for num in xrange(1, self.pages+1):
+            if num <= left_edge or (num > self.page - left_current - 1 and num < self.page + right_current) or num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
