0411381d329cda338b25a6f85d34386d7bedfc06
Yan, Andrew N. <ayan@usgs.gov>
2014-10-30 13:16:31 -0500

    fixed broken test after recent merge

--- pubs_ui/tests/test_utils.py
+++ pubs_ui/tests/test_utils.py
@@ -23,7 +23,7 @@ class TestGetPubsSearchResults(TestCase):
         
         self.search_url = BASE_SEARCH_URL
         self.test_params = {'q': 'some_state'}
-        self.resp_data = [{u'a': 1, u'b': 2, u'c': 3}]
+        self.resp_data = {'records': [{'authors': [{'given': 'apple', 'family': 'orange', 'rank': 1, 'corporation': False},], 'series_number': '18301'}]}
  
     @httpretty.activate
     def test_response_behavior_with_data(self):
@@ -38,8 +38,9 @@ class TestGetPubsSearchResults(TestCase):
                                )
         result = get_pubs_search_results(self.search_url, self.test_params)
         response_content, status_code = result
+        expected_return_content = self.resp_data = {'records': [{'authorList': ['apple orange'], 'authors': [{'given': 'apple', 'family': 'orange', 'rank': 1, 'corporation': False},], 'series_number': '18301'}]}
         self.assertEquals(status_code, 200)
-        self.assertEquals(response_content, self.resp_data)
+        self.assertEquals(response_content, expected_return_content)
         
     @httpretty.activate
     def test_response_behavior_with_down_service(self):
