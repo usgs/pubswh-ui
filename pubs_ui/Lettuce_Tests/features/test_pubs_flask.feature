9d2d7be390fa2f4b9f6ab908b555d89d1b872f7b
skochaver-USGS <skochaver@usgs.gov>
2014-11-07 17:17:44 -0600

    Lettuce coverage completed for flask_tests. One test still failing.

--- pubs_ui/Lettuce_Tests/features/test_pubs_flask.feature
+++ pubs_ui/Lettuce_Tests/features/test_pubs_flask.feature
@@ -6,4 +6,20 @@ Feature: Test Pubs Flask is working
 	Scenairo: Test Index
 		Given I have a mock json
 		When I get the response code with the app
-		I see it's the 200 status code I expected
+		Then I find the status code I expected
+
+	Scenario: Test API Web Args Search Query
+		Given I have state_name query to search
+		When I request context from the app
+		Then I am given state_name as the query parameter
+
+	Scenario: Test API Web Arg Search Response
+		Given I have a mock json
+		When I get the query response code	
+		Then I find the status code I expect
+	
+	Scenario: Test Contact Page
+		Given I have a contact url
+		When I look for a response from the app
+		Then I find the status code I expect
+
--- pubs_ui/Lettuce_Tests/features/test_pubs_flask.py
+++ pubs_ui/Lettuce_Tests/features/test_pubs_flask.py
@@ -1,19 +1,56 @@
 import flask
 import httpretty
-from . import app
 from lettuce import *
+from nose.tools import assert_equal
+import flask
+from pubs_ui import app
 
-world.api_search = 'https://pubs-fake.er.usgs.gov/pubs-services/publication/'
 
 @step(r'I have a mock json')
 def create_first_mock(step):
+    world.api_search = 'https://pubs-fake.er.usgs.gov/pubs-services/publication/'
     httpretty.enable()
     httpretty.register_uri(httpretty.GET, world.api_search,
                            content_type='application/json',
                            status=503)
     
-@step('I get the response code with the app')
+@step(r'I get the response code with the app')
 def app_call(step):
     with app.test_client() as c:
         response = c.get('/')
-        status_code = response.status_code
+        world.status_code = response.status_code
+
+@step(r'I find the status code I expect')
+def asset_status(step):
+    assert_equal(world.status_code, 200)
+    httpretty.reset()
+    httpretty.diable()
+
+@step(r'I have state_name query to search')
+def set_query_uri(step):
+    world.search_url = '/search?q=state_name'
+
+@step(r'I request context from the app')
+def con_text(step):
+    with app.test_request_context(world.search_url):
+        world.query_param = flask.request.args['q']
+
+@step(r'I am given state_name as the query parameter')
+def test_param(step):
+    assert_equal(world.query_param, 'state_name')
+
+@step(r'I get the query response code')
+def query_call(step):
+    with app.test_client() as c:
+        response = c.get(world.search_url)
+    world.status_code = response.status_code
+
+@step(r'Given I have a contact url')
+def contact_url(step):
+    world.contact_url = '/contact'
+
+@step(r'I look for a response from the app')
+def contact_call(step):
+    with app.test_client as c:
+        response = c.get(world.contact_url)
+        world.status_code = response.status_code
