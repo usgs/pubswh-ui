eaa57e76b05cddb5d92e046c47e6800751be9e7e
Yan, Andrew N. <ayan@usgs.gov>
2014-11-10 15:45:04 -0600

    added custom jinja 2 filter

--- pubs_ui/__init__.py
+++ pubs_ui/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask.ext.images import Images
+#from jinja2 import Environment
+from pubs_ui.custom_filters import get_publication_type
 
+#env = Environment()
+#env.filters['getpubtype'] = get_publication_type
 app = Flask(__name__)
 images = Images(app)
 app.config.from_object('settings')
+app.jinja_env.filters['getpubtype'] = get_publication_type
 
 
 import PubsFlask
