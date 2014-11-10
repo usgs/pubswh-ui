1be9cc85d11bd49b926139c0510493bcb085a332
Yan, Andrew N. <ayan@usgs.gov>
2014-11-10 14:59:57 -0600

    added logging

--- runserver.py
+++ runserver.py
@@ -1,5 +1,12 @@
+import logging
 from pubs_ui import app as application
 
 
 if __name__ == '__main__':
+    FORMAT = '%(asctime)s %(message)s'
+    fmt = logging.Formatter(FORMAT)
+    handler = logging.FileHandler('pubs_ui.log')
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(fmt)
+    application.logger.addHandler(handler)
     application.run(port=5050)
