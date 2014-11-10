d15c309b5b3d86619a3f844237dd182128570e1f
Yan, Andrew N. <ayan@usgs.gov>
2014-11-10 15:45:40 -0600

    clean-up of __init__.py

--- pubs_ui/__init__.py
+++ pubs_ui/__init__.py
@@ -3,8 +3,7 @@ from flask.ext.images import Images
 #from jinja2 import Environment
 from pubs_ui.custom_filters import get_publication_type
 
-#env = Environment()
-#env.filters['getpubtype'] = get_publication_type
+
 app = Flask(__name__)
 images = Images(app)
 app.config.from_object('settings')
