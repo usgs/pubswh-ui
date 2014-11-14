33524aa83c7b388947a2db61da8539277052ea4c
Yan, Andrew N. <ayan@usgs.gov>
2014-11-14 15:55:18 -0600

    added included

--- pubs_ui/lettuce_tests/features/terrain.py
+++ pubs_ui/lettuce_tests/features/terrain.py
@@ -19,7 +19,7 @@ def prepare_coverage():
 @after.all
 def collect_coverage(total_results):
     cov.stop()
-    cov.xml_report(outfile='coverage.xml')
+    cov.xml_report(outfile='coverage.xml', include=['pubs_ui/*'])
     scenarios_ran = total_results.scenarios_ran
     scenarios_passed = total_results.scenarios_passed
     message = '{0} scenarios were run, {1} passed'.format(scenarios_ran, scenarios_passed)
