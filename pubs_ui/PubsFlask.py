7656e1c536f89f0a24fdea77cc5d4569f853f024
Yan, Andrew N. <ayan@usgs.gov>
2014-11-20 08:06:39 -0600

    removed print function

--- pubs_ui/PubsFlask.py
+++ pubs_ui/PubsFlask.py
@@ -34,7 +34,6 @@ verify_cert = app.config['VERIFY_CERT']
 @app.route('/')
 def index():
     sp = SearchPublications(search_url)
-    print(search_url)
     recent_publications_resp = sp.get_pubs_search_results(params={'pubs_x_days': 7, 'page_size': 6}) # bring back recent publications
     recent_pubs_content = recent_publications_resp[0]
     try:
