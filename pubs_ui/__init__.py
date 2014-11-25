f5fe5ccf01168f27f11398395e757bdc87d782ff
Yan, Andrew N. <ayan@usgs.gov>
2014-11-25 09:28:23 -0600

    flask-images fussing

--- pubs_ui/__init__.py
+++ pubs_ui/__init__.py
@@ -1,11 +1,21 @@
+import logging
 from flask import Flask
 from flask.ext.images import Images
 from flask_mail import Mail
 from pubs_ui.custom_filters import display_publication_info
 
+FORMAT = '%(asctime)s %(message)s'
+fmt = logging.Formatter(FORMAT)
+handler = logging.FileHandler('pubs_ui.log')
+handler.setLevel(logging.INFO)
+handler.setFormatter(fmt)
+# application.logger.addHandler(handler)
+
 
 app = Flask(__name__)
 app.config.from_object('settings') # load configuration before passing the app object to other things
+if app.config['DEBUG']:
+    app.logger.addHandler(handler)
 images = Images(app)
 mail = Mail(app)
 app.jinja_env.filters['display_pub_info'] = display_publication_info
