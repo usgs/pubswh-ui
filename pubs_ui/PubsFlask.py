f741829f7c1c94ab36e4042086823f1e8d24bf7a
James Kreft <jkreft@usgs.gov>
2014-12-18 16:16:49 -0600

    pretty esri basemaps

--- pubs_ui/PubsFlask.py
+++ pubs_ui/PubsFlask.py
@@ -13,10 +13,9 @@ from canned_text import EMAIL_RESPONSE
 from pubs_ui import app, mail
 import arrow
 
-#set UTF-8 to be default throughout app
+# set UTF-8 to be default throughout app
 reload(sys)
 sys.setdefaultencoding("utf-8")
-
 
 pub_url = app.config['PUB_URL']
 lookup_url = app.config['LOOKUP_URL']
@@ -31,7 +30,7 @@ robots_welcome = app.config.get('ROBOTS_WELCOME')
 json_ld_id_base_url = app.config.get('JSON_LD_ID_BASE_URL')
 google_webmaster_tools_code = app.config.get('GOOGLE_WEBMASTER_TOOLS_CODE')
 
-#should requests verify the certificates for ssl connections
+# should requests verify the certificates for ssl connections
 verify_cert = app.config['VERIFY_CERT']
 
 
@@ -39,9 +38,11 @@ verify_cert = app.config['VERIFY_CERT']
 def robots():
     return render_template('robots.txt', robots_welcome=robots_welcome)
 
-@app.route('/'+google_webmaster_tools_code+'.html')
+
+@app.route('/' + google_webmaster_tools_code + '.html')
 def webmaster_tools_verification():
     return render_template('google_site_verification.html')
+
 
 @app.route('/')
 def index():
@@ -54,20 +55,20 @@ def index():
         for record in pubs_records:
             record = create_display_links(record)
             if replace_pubs_with_pubs_test:
-                record['displayLinks']['Thumbnail'][0]['url'] = change_to_pubs_test(record['displayLinks']['Thumbnail'][0]['url'])
+                record['displayLinks']['Thumbnail'][0]['url'] = change_to_pubs_test(
+                    record['displayLinks']['Thumbnail'][0]['url'])
 
     except TypeError:
-        pubs_records = [] # return an empty list recent_pubs_content is None (e.g. the service is down)
+        pubs_records = []  # return an empty list recent_pubs_content is None (e.g. the service is down)
     form = SearchForm(None, obj=request.args)
     form.advanced.data = True
     return render_template('home.html',
-                           recent_publications=pubs_records, 
+                           recent_publications=pubs_records,
                            form=form,
-                           advanced=request.args.get('advanced')
-                           )
+                           advanced=request.args.get('advanced'))
 
 
-#contact form
+# contact form
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     contact_form = ContactForm()
@@ -79,33 +80,38 @@ def contact():
                 sender_str = '({name}, {email})'.format(name=human_name, email=human_email)
             else:
                 sender_str = '({email})'.format(email=human_email)
-            subject_line = 'Pubs Warehouse User Comments' # this is want Remedy filters on to determine if an email goes to the pubs support group
+            subject_line = 'Pubs Warehouse User Comments'  # this is want Remedy filters on to determine if an email
+            # goes to the pubs support group
             message_body = contact_form.message.data
             message_content = EMAIL_RESPONSE.format(contact_str=sender_str, message_body=message_body)
             msg = Message(subject=subject_line,
                           sender=(human_name, human_email),
-                          reply_to=('PUBSV2_NO_REPLY', 'pubsv2_no_reply@usgs.gov'), # this is not what Remedy filters on to determine if a message goes to the pubs support group...
-                          recipients=contact_recipients, # will go to servicedesk@usgs.gov if application has DEBUG = False
-                          body=message_content
-                          )
-            mail.send(msg)            
-            return redirect(url_for('contact_confirmation')) # redirect to a confirmation page after successful validation and message sending
+                          reply_to=('PUBSV2_NO_REPLY', 'pubsv2_no_reply@usgs.gov'),
+                          # this is not what Remedy filters on to determine if a message
+                          # goes to the pubs support group...
+                          recipients=contact_recipients,
+                          # will go to servicedesk@usgs.gov if application has DEBUG = False
+                          body=message_content)
+            mail.send(msg)
+            return redirect(url_for(
+                'contact_confirmation'))  # redirect to a conf page after successful validation and message sending
         else:
-            return render_template('contact.html', contact_form=contact_form) # redisplay the form with errors if validation fails
+            return render_template('contact.html',
+                                   contact_form=contact_form)  # redisplay the form with errors if validation fails
     elif request.method == 'GET':
         return render_template('contact.html', contact_form=contact_form)
 
-    
+
 @app.route('/contact_confirm')
 def contact_confirmation():
     confirmation_message = 'Thank you for contacting the USGS Publications Warehouse support team.'
     return render_template('contact_confirm.html', confirm_message=confirmation_message)
 
 
-#leads to rendered html for publication page
+# leads to rendered html for publication page
 @app.route('/publication/<indexId>')
 def publication(indexId):
-    r = get(pub_url+'publication/'+indexId, params={'mimetype': 'json'}, verify=verify_cert)
+    r = get(pub_url + 'publication/' + indexId, params={'mimetype': 'json'}, verify=verify_cert)
     if r.status_code == 404:
         return render_template('404.html')
     pubreturn = r.json()
@@ -122,21 +128,22 @@ def publication(indexId):
     # Apache to do the rewrite, this code snippet executes the rewrite so the correct signature is preserved for
     # a given image URL.
     if replace_pubs_with_pubs_test:
-        pubdata['displayLinks']['Thumbnail'][0]['url'] = change_to_pubs_test(pubdata['displayLinks']['Thumbnail'][0]['url'])
+        pubdata['displayLinks']['Thumbnail'][0]['url'] = change_to_pubs_test(
+            pubdata['displayLinks']['Thumbnail'][0]['url'])
     if 'mimetype' in request.args and request.args.get("mimetype") == 'json':
         return jsonify(pubdata)
     else:
         return render_template('publication.html', indexID=indexId, pubdata=pubdata)
 
 
-#leads to json for selected endpoints
+# leads to json for selected endpoints
 @app.route('/lookup/<endpoint>')
 def lookup(endpoint):
     endpoint_list = ['costcenters', 'publicationtypes', 'publicationsubtypes', 'publicationseries']
     endpoint = endpoint.lower()
     if endpoint in endpoint_list:
-        r = get(lookup_url+endpoint, params={'mimetype': 'json'},  verify=verify_cert).json()
-        return Response(json.dumps(r),  mimetype='application/json')
+        r = get(lookup_url + endpoint, params={'mimetype': 'json'}, verify=verify_cert).json()
+        return Response(json.dumps(r), mimetype='application/json')
     else:
         abort(404)
 
@@ -172,20 +179,21 @@ def other_resources():
 @app.route('/browse/', defaults={'path': ''})
 @app.route('/browse/<path:path>')
 def browse(path):
-    app.logger.info("path: "+path)
-    browsecontent = getbrowsecontent(browse_url+path, browse_replace)
+    app.logger.info("path: " + path)
+    browsecontent = getbrowsecontent(browse_url + path, browse_replace)
     return render_template('browse.html', browsecontent=browsecontent)
 
 
-#this takes advantage of the webargs package, which allows for multiple parameter entries. e.g. year=1981&year=1976
+# this takes advantage of the webargs package, which allows for multiple parameter entries. e.g. year=1981&year=1976
 @app.route('/search', methods=['GET'])
 def search_results():
     parser = FlaskParser()
     search_kwargs = parser.parse(search_args, request)
-    form = SearchForm(None, obj=request.args,)
-    #populate form based on parameter
+    form = SearchForm(None, obj=request.args, )
+    # populate form based on parameter
     form.advanced.data = True
-    form_element_list = ['q', 'title', 'contributingOffice', 'contributor', 'typeName', 'subtypeName', 'seriesName', 'reportNumber', 'year']
+    form_element_list = ['q', 'title', 'contributingOffice', 'contributor', 'typeName', 'subtypeName', 'seriesName',
+                         'reportNumber', 'year']
     for element in form_element_list:
         if len(search_kwargs[element]) > 0:
             form[element].data = search_kwargs[element][0]
@@ -197,55 +205,56 @@ def search_results():
         search_kwargs['page_number'] = search_kwargs['page']
 
     sp = SearchPublications(search_url)
-    search_results, resp_status_code = sp.get_pubs_search_results(params=search_kwargs) # go out to the pubs API and get the search results
+    search_results, resp_status_code = sp.get_pubs_search_results(
+        params=search_kwargs)  # go out to the pubs API and get the search results
     try:
         search_result_records = search_results['records']
         record_count = search_results['recordCount']
-        pagination = Pagination(page=int(search_kwargs['page_number']), total=record_count, per_page=int(search_kwargs['page_size']), record_name='Search Results', bs_version=3)
+        pagination = Pagination(page=int(search_kwargs['page_number']), total=record_count,
+                                per_page=int(search_kwargs['page_size']), record_name='Search Results', bs_version=3)
         search_service_down = None
-        start_plus_size = int(search_results['pageRowStart'])+int(search_results['pageSize'])
+        start_plus_size = int(search_results['pageRowStart']) + int(search_results['pageSize'])
         if record_count < start_plus_size:
             record_max = record_count
         else:
             record_max = start_plus_size
 
-        result_summary = {'record_count':record_count, 'page_number':search_results['pageNumber'], 'records_per_page':search_results['pageSize'],
-                          'record_min':(int(search_results['pageRowStart'])+1), 'record_max':record_max }
+        result_summary = {'record_count': record_count, 'page_number': search_results['pageNumber'],
+                          'records_per_page': search_results['pageSize'],
+                          'record_min': (int(search_results['pageRowStart']) + 1), 'record_max': record_max}
     except TypeError:
         search_result_records = None
         pagination = None
         search_service_down = 'The backend services appear to be down with a {0} status.'.format(resp_status_code)
         result_summary = {}
-    return render_template('search_results.html', 
+    return render_template('search_results.html',
                            advanced=search_kwargs['advanced'],
                            result_summary=result_summary,
                            search_result_records=search_result_records,
                            pagination=pagination,
                            search_service_down=search_service_down,
                            form=form
-                           )
+    )
 
 
-   
 @app.route('/site-map')
 def site_map():
     """
     View for troubleshooting application URL rules
     """
     app_urls = []
-    
+
     for url_rule in app.url_map.iter_rules():
         app_urls.append((str(url_rule), str(url_rule.endpoint)))
-    
+
     return render_template('site_map.html', app_urls=app_urls)
 
 
-@app.route('/newpubs', methods = ['GET'])
+@app.route('/newpubs', methods=['GET'])
 def new_pubs():
-
     num_form = NumSeries()
     sp = SearchPublications(search_url)
-    search_kwargs = {'pub_x_days': 30} #bring back recent publications
+    search_kwargs = {'pub_x_days': 30}  #bring back recent publications
 
     if request.args.get('num_series') == 'y':
         num_form = NumSeries(num_series=True)
@@ -253,8 +262,6 @@ def new_pubs():
 
     recent_publications_resp = sp.get_pubs_search_results(params=search_kwargs)
     recent_pubs_content = recent_publications_resp[0]
-
-
 
     try:
         pubs_records = recent_pubs_content['records']
