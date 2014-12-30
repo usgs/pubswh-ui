774eaad2fe11c18b10a4cd56b3a94797599d8d03
skochaver-USGS <skochaver@usgs.gov>
2014-12-30 15:18:27 -0600

    Added select fields for all selectable fields in advance search

--- pubs_ui/forms.py
+++ pubs_ui/forms.py
@@ -21,15 +21,30 @@ class SearchForm(Form):
     title = StringField("Title")
     contributor = StringField("Contributor Name")
     year = StringField("Year Published")
-    contributingOffice = StringField("Contributing Office")
-    typeName = StringField("Publication Type")
-    subtypeName = StringField("Publication Subtype")
-    #seriesName = StringField("Series Name")
 
+    #Grabs the list of Contributing Office names and IDs from the lookup url. Then puts them into the selectfield list
+    contributorList = []
+    for type in get(lookup_url + 'costcenters', params={'mimetype': 'json'}).json():
+        contributorList.append((type['id'], type['text']))
+    contributingOffice = SelectField(label="Contributing Office", choices=sorted(contributorList))
+
+    #Grabs the list of Publication Types names and IDs from the lookup url. Then puts them into the selectfield list
+    typeList = []
+    for type in get(lookup_url + 'publicationtypes', params={'mimetype': 'json'}).json():
+        typeList.append((type['id'], type['text']))
+    typeName = SelectField(label="Type Name", choices=sorted(typeList))
+
+    #Grabs the list of Publication Subtypes names and IDs from the lookup url. Then puts them into the selectfield list
+    subtypeNameList = []
+    for name in get(lookup_url + 'publicationsubtypes', params={'mimetype': 'json'}).json():
+        subtypeNameList.append((name['id'], name['text']))
+    subtypeName = SelectField(label="Subtype Name", choices=sorted(subtypeNameList))
+
+    #Grabs the list of Publication Series names and IDs from the lookup url. Then puts them into the selectfield list
     seriesNameList = []
     for series in get(lookup_url + 'publicationseries', params={'mimetype': 'json'}).json():
         seriesNameList.append((series['id'], series['text']))
-    seriesName = SelectField(label="Series Name", choices=seriesNameList)
+    seriesName = SelectField(label="Series Name", choices=sorted(seriesNameList))
 
     reportNumber = StringField("Report Number")
     advanced = HiddenField('advanced')
