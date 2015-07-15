635e251fbf9d8346d2e4a5b39f6467070825aeca
James Kreft <jkreft@usgs.gov>
2015-07-15 14:32:05 -0500

    ignore case on sorts

--- pubs_ui/utils.py
+++ pubs_ui/utils.py
@@ -629,7 +629,7 @@ def make_chapter_data_for_display(pubdata):
     """
     if len(pubdata.get('interactions')) > 0:
         # natural sort the indexIDs so that chapter 2 comes after chapter one and before chapter three
-        pubdata['interactions'] = natsort.natsorted(pubdata['interactions'], key=lambda x: x['subject']['indexId'])
+        pubdata['interactions'] = natsort.natsorted(pubdata['interactions'], key=lambda x: x['subject']['indexId'], alg=natsort.ns.IGNORECASE)
         # determine wheter to display the publication subparts chunk of the template
         for interaction in pubdata['interactions']:
             if interaction['predicate'] == "IS_PART_OF" and interaction['subject']['indexId'] != pubdata['indexId']:
