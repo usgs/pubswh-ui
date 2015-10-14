9c7f56be6659d89aa44930b055afa9ac8d974ce3
Mary Bucknell <mbucknell@usgs.gov>
2015-10-14 11:53:04 -0500

    Moved the assets to a separate file. Added assets for the new manager app css. Added requirejs dependancy and added an init.js which takes the jquery library as a dependency

---
+++ pubs_ui/manager/static/js/init.js
@@ -0,0 +1,9 @@
+/* jslint browser: true */
+
+define(['jquery'], function($) {
+    $(document).ready(function () {
+        "use strict";
+
+        $('h1').append('<p>Testing javascript</p>');
+    });
+});
