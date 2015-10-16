95f7ec68fc824e23124f642bcbdb3dbbe354c720
Mary Bucknell <mbucknell@usgs.gov>
2015-10-16 14:57:00 -0500

    Added a script which can be run to install the virtualenv and javascript dependencies. Note that it needs to be run in a linux enviroment

---
+++ dev_install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "This script assumes you have python 2.7, pip and virtualenv installed."
+echo "You will also need bower installed (npm install bower)"
+if [ ! -s local_settings.py ]; then
+   echo "Please create a local_settings.py file before proceeding. See the README.md for what's required"
+   exit
+fi
+
+echo "Creating the virtualenv env and installing requirements"
+virtualenv --python=python2.7 env
+. env/bin/activate
+pip install -r requirements.txt
+
+echo "Install javascript dependencies"
+cd pubs_ui
+bower install
+cd ..
+
+echo "Successfully installed the pubs_ui app. "
+echo "When running scripts in this project set up the python interpreter to use env/bin/python."
+echo "The development server can be run as follows: env/bin/python run.py"
+
