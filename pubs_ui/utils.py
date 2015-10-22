5e3bd89f0b8bb8c36168b7a97baa0123a68f13d2
Mary Bucknell <mbucknell@usgs.gov>
2015-10-22 16:01:55 -0500

    Need to use endpoints to resolve redirect url because of our forwarding rules and then use url_for

--- pubs_ui/utils.py
+++ pubs_ui/utils.py
@@ -3,9 +3,17 @@ from urlparse import urlparse, urljoin
 
 from . import app
 
-def get_url_rule(url, wsgi_str=None):
+def get_url_endpoint(url, fallback, wsgi_str=None):
     if not wsgi_str:
         wsgi_str = app.config['WSGI_STR']
+    this_rule = url.replace(wsgi_str, '')
+
+    for rule in app.url_map.iter_rules():
+        if str(rule) == this_rule:
+            return rule.endpoint
+
+    return fallback
+
 
     return url.replace(wsgi_str, '')
 
