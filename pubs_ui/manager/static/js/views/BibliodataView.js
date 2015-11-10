14d6ffde02dfe3c35c665320f28b489077541e24
Mary Bucknell <mbucknell@usgs.gov>
2015-11-10 16:37:56 -0600

    Setup tinymce for the docAbstract. Backbone.stickit does not work for this and I couldn't get the jquery version to load with require. Should probably look at creating a tinymce view which would handle some of the boilerplate.

--- pubs_ui/manager/static/js/views/BibliodataView.js
+++ pubs_ui/manager/static/js/views/BibliodataView.js
@@ -4,13 +4,14 @@ define([
 	'handlebars',
 	'jquery',
 	'select2',
+	'tinymce',
 	'backbone.stickit',
 	'models/PublicationTypeCollection',
 	'models/CostCenterCollection',
 	'views/BaseView',
 	'text!hb_templates/bibliodata.hbs',
 	'module'
-], function(Handlebars, $, select2, stickit, PublicationTypeCollection, CostCenterCollection, BaseView, hbTemplate, module) {
+], function(Handlebars, $, select2, tinymce, stickit, PublicationTypeCollection, CostCenterCollection, BaseView, hbTemplate, module) {
 	"use strict";
 
 	var view = BaseView.extend({
@@ -40,7 +41,6 @@ define([
 			'#chapter-input' : 'chapter',
 			'#subchapter-input' : 'subchapterNumber',
 			'#title-input' : 'title',
-			'#docAbstract-input' : 'docAbstract',
 			'#usgs-citation-input' : 'usgsCitation',
 			'#collaboration-input' : 'collaboration',
 			'#larger-work-title-input' : 'largerWorkTitle',
@@ -65,9 +65,47 @@ define([
 		render : function() {
 			var self = this;
 			BaseView.prototype.render.apply(this, arguments);
+			this.updateDocAbstract();
+			tinymce.init({
+				selector : '#docAbstract-input',
+				setup : function (ed) {
+          			ed.on('change', function(ev) {
+						self.model.set('docAbstract', ev.level.content);
+          			});
+   				},
+				menubar: false,
+				plugins : 'code link paste',
+				formats: {
+						italic: {inline: 'i'}
+					},
+				browser_spellcheck : true,
+				toolbar : 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | subscript superscript | link | code',
+				valid_elements : "@[id|class|title|dir<ltr?rtl|lang|xml::lang|onclick|ondblclick|"
+						+ "onmousedown|onmouseup|onmouseover|onmousemove|onmouseout|onkeypress|"
+						+ "onkeydown|onkeyup],a[rel|rev|charset|hreflang|tabindex|accesskey|type|"
+						+ "name|href|target|title|class|onfocus|onblur],strong/b,i/em,strike,u,"
+						+ "#p,-ol[type|compact],-ul[type|compact],-li,br,img[longdesc|usemap|"
+						+ "src|border|alt=|title|hspace|vspace|width|height|align],-sub,-sup,"
+						+ "-blockquote,-table[border=0|cellspacing|cellpadding|width|frame|rules|"
+						+ "height|align|summary|bgcolor|background|bordercolor],-tr[rowspan|width|"
+						+ "height|align|valign|bgcolor|background|bordercolor],tbody,thead,tfoot,"
+						+ "#td[colspan|rowspan|width|height|align|valign|bgcolor|background|bordercolor"
+						+ "|scope],#th[colspan|rowspan|width|height|align|valign|scope],caption,-div,"
+						+ "-span,-code,-pre,address,-h1,-h2,-h3,-h4,-h5,-h6,hr[size|noshade],-font[face"
+						+ "|size|color],dd,dl,dt,cite,abbr,acronym,del[datetime|cite],ins[datetime|cite],"
+						+ "object[classid|width|height|codebase|*],param[name|value|_value],embed[type|width"
+						+ "|height|src|*],script[src|type],map[name],area[shape|coords|href|alt|target],bdo,"
+						+ "button,col[align|char|charoff|span|valign|width],colgroup[align|char|charoff|span|"
+						+ "valign|width],dfn,fieldset,form[action|accept|accept-charset|enctype|method],"
+						+ "input[accept|alt|checked|disabled|maxlength|name|readonly|size|src|type|value],"
+						+ "kbd,label[for],legend,noscript,optgroup[label|disabled],option[disabled|label|selected|value],"
+						+ "q[cite],samp,select[disabled|multiple|name|size],small,"
+						+ "textarea[cols|rows|disabled|name|readonly],tt,var,big"
+			});
 
 			// Sets up the binding between DOM elements and the model //
 			this.stickit();
+
 			this.$('#pub-type-input').select2({
 				allowClear: true
 			});
@@ -216,6 +254,7 @@ define([
 			this.listenTo(this.model, 'change:costCenters', this.updateCostCenters);
 			this.listenTo(this.model, 'change:largerWorkType', this.updateLargerWorkType);
 			this.listenTo(this.model, 'change:largerWorkSubtype', this.updateLargerWorkSubtype);
+			this.listenTo(this.model, 'change:docAbstract', this.updateDocAbstract);
 		},
 
 		selectPubType : function(ev) {
@@ -386,6 +425,10 @@ define([
 			else {
 				$select.val('').trigger('change');
 			}
+		},
+
+		updateDocAbstract : function() {
+			this.$('#docAbstract-input').html(this.model.get('docAbstract'));
 		}
 
 	});
