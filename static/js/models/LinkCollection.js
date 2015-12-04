823d49f41abd95b2a55729b1962503b66dadb96e
Tomcat Application Server <tomcat@cida-eros-pubsdev.er.usgs.gov>
2015-12-04 10:35:47 -0600

    automatic inclusion of the env and static files

---
+++ static/js/models/LinkCollection.js
@@ -0,0 +1,14 @@
+/* jslint browser: true */
+
+define([
+	'models/LinkModel',
+	'models/OrderedCollection'
+], function(LinkModel, OrderedCollection) {
+	"use strict";
+
+	var collection = OrderedCollection.extend({
+		model : LinkModel,
+	});
+
+	return collection;
+});
