45c77c2eddb0a33f42105e6f7bd7aea562cd17ca
Mary Bucknell <mbucknell@usgs.gov>
2016-01-06 15:38:55 -0600

    Updated the install script and README.md with new instructions on how to run the karma tests

--- dev_install.sh
+++ dev_install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 echo "This script assumes you have python 2.7, pip and virtualenv installed."
-echo "You will also need bower and lessc installed (npm install -g bower and npm install -g less)"
+echo "You will also need to have bower and lessc installed (npm install -g bower and npm install -g less)"
+echo "Also recommend that you install karma-cli which allows you to more easily run karma tests by not having to specify the path to karma
 if [ ! -s instance/config.py ]; then
    echo "Please create a instance/config.py file before proceeding. See the README.md for what's required"
    exit
@@ -11,6 +12,10 @@ echo "Creating the virtualenv env and installing requirements"
 virtualenv --python=python2.7 env
 . env/bin/activate
 pip install -r requirements.txt
+
+echo "Install test runner and karma command line interface"
+npm install
+
 
 echo "Install javascript dependencies"
 bower install
