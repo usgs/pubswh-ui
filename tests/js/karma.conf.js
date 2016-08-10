20b7938969d1d5e5f285c8dd01957d17f5768960
Mary Bucknell <mbucknell@usgs.gov>
2016-08-10 14:02:47 -0500

    Updated the maven dependencies as well as the package.json dependencies. Added the ability to clean the project from maven and beefed up the install script so that the user can specify clean and test (--clean and --test)

--- tests/js/karma.conf.js
+++ tests/js/karma.conf.js
@@ -57,7 +57,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots', 'coverage'],
 
     coverageReporter: {
       reporters : [
@@ -85,12 +85,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
