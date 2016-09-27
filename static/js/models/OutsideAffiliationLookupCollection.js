72241a3d8465972b1a1bd0fc357bb6e87782aee5
Tomcat Application Server <tomcat@cida-eros-pubsdev.er.usgs.gov>
2016-09-27 11:19:17 -0500

    automatic inclusion of the env and static files

---
+++ static/js/models/OutsideAffiliationLookupCollection.js
@@ -0,0 +1,18 @@
+/* jslint browser:true */
+/* global define */
+
+define([
+	'backbone',
+	'models/LookupModel',
+	'module'
+], function(Backbone, LookupModel, module) {
+	"use strict";
+
+	var collection = Backbone.Collection.extend({
+		model: LookupModel,
+		url: module.config().lookupUrl + 'outsideaffiliates?mimetype=json',
+	});
+
+	return collection;
+
+});
