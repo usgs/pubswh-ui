eb06d5b20481bc56560241acc9a059c31fa369d6
Yan, Andrew N <ayan@usgs.gov>
2016-11-11 10:46:17 -0600

    add BOWER_TRY_MINIFIED back into config.py

--- config.py
+++ config.py
@@ -34,6 +34,8 @@ REDIS_CONFIG = ''
 IMAGE_CACHE = ''  # path to image cache for thumbnails
 SCIENCEBASE_PARENT_UUID = '' #set to the sciecebase folder id for the core publications warehouse SB folder
 
+BOWER_TRY_MINIFIED = False  # set to solve problem with backgrid-paginator
+
 # Config for Flask-Assets
 ASSETS_DEBUG = False  # to disable compression of js and css set to True
 ASSETS_AUTO_BUILD = False  # Local developers will typically set this to True in their instance/config.py.
