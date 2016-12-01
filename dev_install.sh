a432dc6b51a78109af65a56b45034899406b3856
Mary Bucknell <mbucknell@usgs.gov>
2016-12-01 11:34:20 -0600

    Run lettuce tests last, because they fail on my local machine unless I install the requirements differently.

--- dev_install.sh
+++ dev_install.sh
@@ -24,10 +24,10 @@ bower install;
 
 echo "Successfully installed the pubs_ui app. "
 echo "Run all tests"
-env/bin/lettuce --with-xunit pubs_ui/lettuce_testing;
-env/bin/nosetests -w pubs_ui;
 node_modules/karma/bin/karma start tests/js/manager/karma.conf.js
 node_modules/karma/bin/karma start tests/js/metrics/karma.conf.js
+env/bin/nosetests -w pubs_ui;
+env/bin/lettuce --with-xunit pubs_ui/lettuce_testing;
 
 if [ ! -s instance/config.py ]; then
    echo "Please create a instance/config.py file before proceeding. See the README.md for what's required"
