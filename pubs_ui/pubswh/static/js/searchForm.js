3fc9cd5a09be8a0811b09a105e69ac941c5c7afd
Mary Bucknell <mbucknell@usgs.gov>
2017-04-06 14:41:04 -0500

    Initial implementation of a dynamically built search form.

--- pubs_ui/pubswh/static/js/searchForm.js
+++ pubs_ui/pubswh/static/js/searchForm.js
@@ -1,3 +1,103 @@
 /* jslint browser: true */
 /* global $ */
+/* global Handlebars */
+/* global CONFIG */
+/* global console */
 
+var PUBS_WH = PUBS_WH || {};
+
+/*
+ * Initially creates inputs to be be used for the advanced search form.
+ * @param {Object} options
+ * 		@prop {Jquery element} $container - Where the advanced search rows will be added
+ * 		@prop {Array of Objects} initialRows - Each object will contain the following properties
+ * 			@prop {String} name - to be used for the name attribute when creating the input
+ * 			@prop {String} displayName - to be used to identify the input
+ * 			@prop {String} inputType - can be any valid html5 input type or select
+ * 			@prop {String} value - The value for this input.
+ * 			@prop {String} lookup (optional) - Used for select inputType to fill in the options via the lookup web service
+ * 	@returns {Object}
+ * 		@prop {Function} addRow - takes single object parameter which has the same properties as the objects in initialRows
+ */
+PUBS_WH.advancedSearchForm = function(options) {
+	"use strict";
+
+	var self = {};
+
+	var ROW_HTML =
+		'<div class="form-group">' +
+		'<div class="col-sm-3">' +
+		'<span class="fa fa-minus-circle delete-row"></span>' +
+		'<label class="">{{row.displayName}}:</label>' +
+		'</div>' +
+		'<div class="col-sm-8">' +
+		'{{#if isSelect}}<select class="form-control" name="{{row.name}}">' +
+		'{{#each options}}<option {{#if selected }}selected{{/if}} value="{{text}}">{{text}}</option>{{/each}}' +
+		'</select>' +
+		'{{else}}' +
+		'<input class="form-control" type="{{row.inputType}}" name="{{row.name}}" value="{{row.value}}" />' +
+		'{{/if}}' +
+		'</div>' +
+		'</div>';
+
+	var rowTemplate = Handlebars.compile(ROW_HTML);
+
+	/*
+	 * Adds an input to options.$container. Takes the information in row to create the specified input
+	 * @param {Object} row
+	 * 		@prop {String} name - to be used for the name attribute when creating the input
+	 * 		@prop {String} displayName - to be used to identify the input
+	 * 		@prop {String} inputType - can be any valid html5 input type or select
+ 	 * 		@prop {String} value (optional) - The value for this input.
+ 	 * 		@prop {String} lookup (optional) - Used for select inputType to fill in the options via the lookup web service
+	 */
+	self.addRow = function(row) {
+		var lookupDeferred = $.Deferred();
+		var lookupOptions = [];
+
+		if ((row.inputType === 'select') && row.lookup) {
+			$.ajax({
+				url: CONFIG.lookupUrl + row.lookup,
+				method: 'GET',
+				success : function(resp) {
+					lookupOptions = resp.map(function(option) {
+						var result = option;
+						result.selected = row.value ? (option.text === row.value) : false;
+						return result;
+					});
+
+					lookupDeferred.resolve();
+				},
+				error : function() {
+					console.log('Lookup did not succeed. Select will be empty');
+					lookupDeferred.resolve();
+				}
+			});
+		}
+		else {
+			lookupDeferred.resolve();
+		}
+
+		lookupDeferred.done(function() {
+			var context = {
+				isSelect : row.inputType === 'select',
+				row : row,
+				options : lookupOptions
+			};
+			options.$container.append(rowTemplate(context));
+			if (context.isSelect) {
+				options.$container.find('select[name="' + context.row.name + '"]').select2();
+			}
+			$('.delete-row').click(function() {
+				$(this).parents('.form-group').remove();
+			});
+		});
+
+
+	};
+
+	//Initialize row
+	options.initialRows.forEach(self.addRow);
+
+	return self;
+};
---
+++ pubs_ui/pubswh/static/js/searchFormOnReady.js
@@ -0,0 +1,51 @@
+/* jslint browser: true */
+/* global $ */
+/* global PUBS_WH */
+/* global CONFIG */
+
+$(document).ready(function() {
+	"use strict";
+
+	var initialSearchRows = [];
+	var $form = $('#search-pubs-form');
+	var $categorySelect = $form.find('.search-category-select');
+	var $addCategoryBtn = $form.find('.add-search-category');
+	var advancedSearchForm;
+
+	// Retrieve information to create the initial search rows from the query parameters and search category list.
+	if (CONFIG.requestArgs !== {}) {
+		$.each(CONFIG.requestArgs, function (name, value) {
+			var $thisOption = $categorySelect.find('option[value="' + name + '"]');
+			if ($thisOption.length) {
+				initialSearchRows.push({
+					name: name,
+					displayName: $thisOption.html(),
+					inputType: $thisOption.data('input-type'),
+					value: value,
+					lookup: $thisOption.data('lookup')
+				});
+			}
+		});
+	}
+
+	advancedSearchForm = PUBS_WH.advancedSearchForm ({
+		$container : $form.find('.advanced-search-container'),
+		initialRows : initialSearchRows
+	});
+
+	$categorySelect.select2();
+	$categorySelect.change(function() {
+		$addCategoryBtn.prop('disabled',  !$(this).val());
+	});
+
+	$addCategoryBtn.click(function() {
+		var $selectedOption = $categorySelect.find('option:selected');
+		advancedSearchForm.addRow({
+			name: $selectedOption.val(),
+			displayName: $selectedOption.html(),
+			inputType: $selectedOption.data('inputType'),
+			lookup: $selectedOption.data('lookup')
+		});
+	});
+
+});
