797b41f5895146898a46b9d16f1b77509c437c13
Mary Bucknell <mbucknell@usgs.gov>
2017-05-09 10:57:30 -0500

    Revert change to karma.conf.js. Updated all dependencies in package.json.

--- tests/js/manager/karma.conf.js
+++ tests/js/manager/karma.conf.js
@@ -59,7 +59,7 @@ module.exports = function(config) {
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      'pubs_ui/manager/static/js/**/*.js': ['coverage']
+      'pubs_ui/manager/static/js/**/*.js': sourcePreprocessors
     },
 
     // test results reporter to use
