ebd57eb858c1cfd2dec93066b0baca15e58b14d3
Daniel Naab <dnaab@usgs.gov>
2018-06-14 10:38:23 -0500

    Switch less compilation to npm run scripts

--- server/pubs_ui/assets.py
+++ server/pubs_ui/assets.py
@@ -10,52 +10,6 @@ base_metrics_js = Bundle(
     'metrics/js/dataUtils.js',
 )
 bundles={
-    'usgs_style' : Bundle(
-        '../../../assets/styles/manager/usgs_header_footer.less',
-        filters='less,cssmin',
-        output='gen/usgs_style.css'
-    ),
-    'css_base' : Bundle(
-        'pubswh/css/normalize.css',
-        'pubswh/css/main.css',
-        filters='cssmin',
-        output='gen/min_base.css'
-    ),
-    'auth_style' : Bundle(
-        '../../../assets/styles/auth.less',
-        depends=[
-            '../../../assets/styles/manager/usgs_header_footer.less'
-        ],
-        filters='less,cssmin',
-        output='gen/auth_style.css'
-    ),
-    'manager_style' : Bundle(
-        '../../../assets/styles/manager/manager_custom.less',
-        depends=[
-            '../../../assets/styles/manager/usgs_header_footer.less',
-            '../../../assets/styles/manager/manage_publications.less',
-            '../../../assets/styles/manager/publication.less',
-            '../../../assets/styles/manager/bibliodata.less',
-            '../../../assets/styles/manager/links.less',
-            '../../../assets/styles/manager/contributors.less',
-            '../../../assets/styles/manager/spn.less',
-            '../../../assets/styles/manager/cataloging.less',
-            '../../../assets/styles/manager/geospatial.less',
-            '../../../assets/styles/manager/editSeriesTitle.less',
-            '../../../assets/styles/manager/manageContributors.less',
-            '../../../assets/styles/manager/manageAffiliations.less'
-        ],
-        filters='less,cssmin',
-        output='gen/manager_style.css'
-    ),
-    'metrics_style' : Bundle(
-        '../../../assets/styles/metrics/metrics_custom.less',
-        depends=[
-            '../../../assets/styles/manager/usgs_header_footer.less',
-        ],
-        filters='less,cssmin',
-        output='gen/metrics_style.css'
-    ),
 }
 
 assets = Environment(app)
