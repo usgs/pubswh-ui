1be1c71ff3505e1312e4943d6c0b841a33f05d16
Daniel Naab <dnaab@usgs.gov>
2018-06-22 14:34:29 -0500

    Add bower_components to coverage excludes

--- assets/tests/js/metrics/karma.conf.js
+++ assets/tests/js/metrics/karma.conf.js
@@ -103,7 +103,8 @@ module.exports = function(config) {
                     istanbul({
                         exclude: [
                             'tests/**/*.js',
-                            'node_modules/**/*.js'
+                            'node_modules/**/*.js',
+                            'bower_components/**/*.js'
                         ]
                     })
                 ]
