f113c312dd91eee34394738d7c49f8f53ed37b5c
Daniel Naab <dnaab@usgs.gov>
2018-06-28 13:48:26 -0500

    Code-splitting initial implementation

--- assets/rollup.config.js
+++ assets/rollup.config.js
@@ -3,93 +3,38 @@
  * NOTE: This is a CommonJS module so it can be imported by Karma.
  */
 
-const buble = require('rollup-plugin-buble');
-const commonjs = require('rollup-plugin-commonjs');
-const handlebars = require('rollup-plugin-handlebars-plus');
-const inject = require('rollup-plugin-inject');
-const json = require('rollup-plugin-json');
-const resolve = require('rollup-plugin-node-resolve');
-const replace = require('rollup-plugin-replace');
-const { uglify } = require('rollup-plugin-uglify');
+const getRollupPlugins = require('./rollup.plugins');
 
 
 const ENV = process.env.NODE_ENV || 'development';
 
-const getBundleConfig = function (src, dest) {
+const getBundleConfig = function (output) {
     return {
-        input: src,
-        plugins: [
-            inject({
-                include: '**/*.js',
-                //exclude: 'node_modules/**',
-                modules: {
-                    $: 'jquery',
-                    jQuery: 'jquery',
-                    moment: 'moment'
-                }
-            }),
-            resolve({
-                // use "module" field for ES6 module if possible
-                module: true, // Default: true
-
-                // use "jsnext:main" if possible
-                // – see https://github.com/rollup/rollup/wiki/jsnext:main
-                jsnext: false,
-
-                // use "main" field or index.js, even if it's not an ES6 module
-                // (needs to be converted from CommonJS to ES6
-                // – see https://github.com/rollup/rollup-plugin-commonjs
-                main: false,  // Default: true
-
-                // some package.json files have a `browser` field which
-                // specifies alternative files to load for people bundling
-                // for the browser. If that's you, use this option, otherwise
-                // pkg.browser will be ignored
-                browser: false  // Default: false
-            }),
-            json(),
-            commonjs(),
-            handlebars({
-                handlebars: {
-                    options: {
-                        sourceMap: ENV !== 'production' ? 'inline': false
-                    }
-                },
-                templateExtension: '.hbs'
-            }),
-            buble({
-                objectAssign: 'Object.assign',
-                transforms: {
-                    dangerousForOf: true
-                }
-            }),
-            replace({
-              'process.env.NODE_ENV': JSON.stringify(ENV)
-            }),
-            ENV === 'production' && uglify({
-                compress: {
-                    dead_code: true,
-                    drop_console: true
-                }
-            })
+        input: [
+            'scripts/manager/manager.js',
+            'scripts/pubswh/extentsMapOnReady.js',
+            'scripts/pubswh/pubswh_plugins.js',
+            'scripts/pubswh/resultsMap.js',
+            'scripts/pubswh/searchFormOnReady.js',
+            'scripts/metrics/pubGraphController.js',
+            'scripts/metrics/pubsGraphController.js',
+            'scripts/metrics/pubsAcquisitionGraphController.js'
         ],
-        output: {
-            name: 'pubs_bundle',
-            file: dest,
-            format: 'iife',
-            sourcemap: ENV !== 'production' ? 'inline': false
-        },
-        treeshake: ENV === 'production'
+        experimentalCodeSplitting: true,
+        plugins: getRollupPlugins(ENV),
+        output
     };
 };
 
 module.exports = [
-    getBundleConfig('scripts/manager/init.js', 'dist/scripts/manager.js'),
-    getBundleConfig('scripts/pubswh/extentsMapOnReady.js', 'dist/scripts/extentsMapOnReady.js'),
-    getBundleConfig('scripts/pubswh/plugins.js', 'dist/scripts/base_pubswh.js'),
-    getBundleConfig('scripts/pubswh/resultsMap.js', 'dist/scripts/resultsMap.js'),
-    getBundleConfig('scripts/pubswh/searchFormOnReady.js', 'dist/scripts/advanced_search.js'),
-    getBundleConfig('scripts/metrics/pubGraphController.js', 'dist/scripts/metrics_publication.js'),
-    getBundleConfig('scripts/metrics/pubsGraphController.js', 'dist/scripts/metrics_publications.js'),
-    getBundleConfig('scripts/metrics/pubsAcquisitionGraphController.js', 'dist/scripts/metrics_publications_acquisition.js')
+    getBundleConfig({
+        dir: 'dist/scripts/module',
+        format: 'es',
+        sourcemap: ENV !== 'production'
+    }),
+    getBundleConfig({
+        dir: 'dist/scripts/nomodule',
+        format: 'system',
+        sourcemap: ENV !== 'production'
+    })
 ];
