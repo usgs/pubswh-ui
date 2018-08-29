1a3d460d48fb834cd79d55de3cec1819393d9b06
Aaron Briggs <abriggs@contractor.usgs.gov>
2018-08-29 14:44:14 -0500

    Changing for loop to foreach, and adding additional tests for empty and non duplicated arrays

--- assets/scripts/pubswh/utils.js
+++ assets/scripts/pubswh/utils.js
@@ -1,22 +1,24 @@
 /**
  * Takes an array of objects, checks for duplicate values of a given property, then
  * creates a new array that excludes the objects containing the duplicated values
- * @param {Array}  arrayWithDuplicates - an array of objects that have properties with duplicate values
+ * @param {Array}  arrayWithDuplicates - an array of objects that have properties with possible duplicate values
  * @param {String} propertyName - name of property to check for duplicate values
  * @returns {Array} deduplicatedArray - an array of objects that have NO properties with duplicate values
  */
-
 export function removeObjectsWithDuplicateValues(arrayWithDuplicates, propertyName) {
     let deduplicatedArray = [];
-    for (let i = 0; i < arrayWithDuplicates.length; i++) {
+    let indexOfObject;
+
+    arrayWithDuplicates.forEach(function(objectFromArrayWithDuplicates) {
         // if the value for the current index is not in the new list that has no duplicates, function will return -1
-        let indexOfDuplicate = deduplicatedArray.findIndex(objectFromArray => objectFromArray.text === arrayWithDuplicates[i][propertyName]);
+        indexOfObject = deduplicatedArray.findIndex(object => object[propertyName] === objectFromArrayWithDuplicates[propertyName]);
+
         // if the value is not in the new list that has no duplicates add the object containing that value to the list,
         // otherwise do nothing
-            if (indexOfDuplicate === -1) {
-                            deduplicatedArray.push(arrayWithDuplicates[i]);
-            }
-    }
+        if (indexOfObject === -1) {
+            deduplicatedArray.push(objectFromArrayWithDuplicates);
+        }
+    });
 
     return deduplicatedArray;
 }
--- assets/tests/js/pubswh/utilsSpec.js
+++ assets/tests/js/pubswh/utilsSpec.js
@@ -1,11 +1,22 @@
 import {removeObjectsWithDuplicateValues} from '../../../scripts/pubswh/utils';
 
-describe('Utils', function() {
+describe('The removeObjectsWithDuplicateValues function test', function() {
 
-    var sampleArrayWithDuplicates = [{'id':1,'text':' duplicated listing','selected':false},{'id':2,'text':' duplicated listing','selected':false}];
+    let sampleArrayWithDuplicates = [{'id':1,'text':' duplicated listing','selected':false},{'id':2,'text':' duplicated listing','selected':false}];
+    let sampleArrayWithoutDuplicates = [{'id':1,'text':' unique listing 1','selected':false},{'id':2,'text':' unique listing 2','selected':false}];
+    let sampleArrayEmpty = [];
 
-    it('expects if given an array with duplicates, it will return an array without duplicates', function () {
-       expect(sampleArrayWithDuplicates.length).toEqual(2);
+    it('expects if given an array with duplicates, it will return an array without duplicates', function() {
        expect(removeObjectsWithDuplicateValues(sampleArrayWithDuplicates, 'text').length).toEqual(1);
+    });
+
+    it('expects if given an array with NO duplicates, it will return the same array', function() {
+        expect(removeObjectsWithDuplicateValues(sampleArrayWithoutDuplicates, 'text').length).toEqual(2);
+        expect(removeObjectsWithDuplicateValues(sampleArrayWithoutDuplicates, 'text')[0].id).toEqual(1);
+        expect(removeObjectsWithDuplicateValues(sampleArrayWithoutDuplicates, 'text')[1].id).toEqual(2);
+    });
+
+    it('expects if given an empty array, it will return an empty array', function() {
+        expect(removeObjectsWithDuplicateValues(sampleArrayEmpty, 'text').length).toEqual(0);
     });
 });
