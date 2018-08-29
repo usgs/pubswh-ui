39627403221275025c80694664c463d791cc426e
Aaron Briggs <abriggs@contractor.usgs.gov>
2018-08-29 12:19:06 -0500

    fixing capitalization of a variable name

--- assets/tests/js/pubswh/utilsSpec.js
+++ assets/tests/js/pubswh/utilsSpec.js
@@ -2,10 +2,10 @@ import {removeObjectsWithDuplicateValues} from '../../../scripts/pubswh/utils';
 
 describe('Utils', function() {
 
-    var sampleArrayWIthDuplicates = [{'id':1,'text':' duplicated listing','selected':false},{'id':2,'text':' duplicated listing','selected':false}];
+    var sampleArrayWithDuplicates = [{'id':1,'text':' duplicated listing','selected':false},{'id':2,'text':' duplicated listing','selected':false}];
 
     it('expects if given an array with duplicates, it will return an array without duplicates', function () {
-       expect(sampleArrayWIthDuplicates.length).toEqual(2);
-       expect(removeObjectsWithDuplicateValues(sampleArrayWIthDuplicates, 'text').length).toEqual(1);
+       expect(sampleArrayWithDuplicates.length).toEqual(2);
+       expect(removeObjectsWithDuplicateValues(sampleArrayWithDuplicates, 'text').length).toEqual(1);
     });
 });
