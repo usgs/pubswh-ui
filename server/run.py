b7188846e22052167b7458b6a01e6ad621e7af31
Mary Bucknell <mbucknell@usgs.gov>
2019-04-29 15:57:55 -0500

    Added new config parameter SECURE_COOKIES which defaults to True. In development, if you are running the dev server on http then set this to False. Also added a --port parameter to run so that the development server can run on a port other than 5050.

--- server/run.py
+++ server/run.py
@@ -6,12 +6,18 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', '-ht', type=str)
+    parser.add_argument('--port', '-p', type=int)
     args = parser.parse_args()
     host_val = args.host
     if host_val is not None:
         host = host_val
     else:
         host = '127.0.0.1'
-    application.run(host=host, port=5050, threaded=True)
+
+    if args.port is None:
+        port = 5050
+    else:
+        port = args.port
+    application.run(host=host, port=port, threaded=True)
     # run from the command line as follows
     # python runserver.py -ht <ip address of your choice>
