242cbd085f7f08830dffacc1a9140071468681de
Mary Bucknell <mbucknell@usgs.gov>
2019-05-09 14:26:47 -0500

    Add auth/logout to pubs services when the user clicks the logout button.

--- server/pubs_ui/auth/views.py
+++ server/pubs_ui/auth/views.py
@@ -3,6 +3,8 @@ from functools import wraps
 import time
 from urllib.parse import urlencode
 
+from requests import Request, Session
+
 from flask import redirect, url_for, Blueprint, session, request
 
 from .. import app, oauth
@@ -88,6 +90,13 @@ def logout():
     Logouts the user and clears the session data
     :return:
     '''
+    # Send logout to pubs services
+    url = '%sauth/logout/' % (app.config['PUB_URL'])
+    proxy_request = Request(method='POST',
+                            url=url)
+    app.logger.info('Logout sent to server %s', url)
+    resp = Session().send(proxy_request.prepare(), verify=app.config['VERIFY_CERT'])
+
     # Clear session stored data
     session.clear()
 
